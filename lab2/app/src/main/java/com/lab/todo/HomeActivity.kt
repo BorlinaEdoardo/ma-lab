265970d1756c00dfe4c3f02b4e9f27684279e35f
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-09-16 16:03:57 -0400

    fix: fixing some compatibility errors

--- lab2/app/src/main/java/com/lab/todo/HomeActivity.kt
+++ lab2/app/src/main/java/com/lab/todo/HomeActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todo
+package com.lab.todo
 
 import android.content.Intent
 import android.os.Bundle
--- lab2/app/src/main/java/com/lab/todo/MainActivity.kt
+++ lab2/app/src/main/java/com/lab/todo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todo
+package com.lab.todo
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
