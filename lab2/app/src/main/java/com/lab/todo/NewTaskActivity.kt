e0779a11ddd4b4f82a20e5f81f59d1365be6677e
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-09-16 16:04:48 -0400

    feat: add new_task activity file

---
+++ lab2/app/src/main/java/com/lab/todo/NewTaskActivity.kt
@@ -0,0 +1,10 @@
+package com.lab.todo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+
+class NewTaskActivity : AppCompatActivity {
+}
