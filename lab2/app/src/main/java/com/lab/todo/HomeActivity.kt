fed948fa9a0dc457b48c279d3ea006ae1d02f305
raptor764 <103536828+raptor764@users.noreply.github.com>
2025-09-16 15:22:37 -0400

    george's commit

---
+++ lab2/app/src/main/java/com/lab/todo/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.todo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val addTaskBtn: Button = findViewById(R.id.addTaskBtn)
+
+        // Navigate to NewTaskActivity
+      //  addTaskBtn.setOnClickListener {
+      //      val intent = Intent(this, NewTaskActivity::class.java)
+      //      startActivity(intent)
+    //    }
+    }
+}
