f7c8bf693cf571faac0de8fe610cf8ceffe23fa4
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-09-30 01:30:59 -0400

    integrating the new features

--- lab2/app/src/main/java/com/lab/todo/NewTaskActivity.kt
+++ lab2/app/src/main/java/com/lab/todo/NewTaskActivity.kt
@@ -1,19 +1,34 @@
 package com.lab.todo
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.DatePicker
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.EditText
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class NewTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_task)
 
-        //val userDate = findViewById<DatePicker>(R.id.editTextDate)
 
-        //val enteredText = userDate.text.toString()
+        val titleInput: EditText = findViewById(R.id.taskTitle)
+        val deadlinePicker: DatePicker = findViewById(R.id.taskDeadline)
+        val descriptionInput: EditText = findViewById(R.id.taskDescription)
+        val doneBtn: Button = findViewById(R.id.doneBtn)
 
+
+        val topAppBar: MaterialToolbar = findViewById(R.id.topAppBar)
+        topAppBar.setNavigationOnClickListener {
+            finish()
+        }
+
+        // Done Button
+        doneBtn.setOnClickListener {
+            val title = titleInput.text.toString().ifBlank { "Untitled" } // fallback title
+
+            finish()
+        }
     }
 }
